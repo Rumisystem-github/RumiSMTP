248ad4b4af482b4856bd9eafd7ab72535d69dbf1
SINtyanneru <irjitgd@gmail.com>
2024-09-09 09:44:46 +0900

    クラス化＆pom設定

---
+++ src/main/java/com/rumisystem/rumi_smtp/Main.java
@@ -0,0 +1,12 @@
+package com.rumisystem.rumi_smtp;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			//TRANSFER TF = new TRANSFER("noreply@rumiserver.com", "RumiSaabaa", "irjitgd@gmail.com");
+			//TF.SEND_MAIL("テスト", "メール送信テスト");
+		} catch (Exception EX) {
+			EX.printStackTrace();
+		}
+	}
+}
